e0f4fc72cd932d3870f7b934b6d2b767f23a3301
wangjian <zgsddzwj@163.com>
2023-10-17 19:45:00 +0800

    Improve agent monitoring with configurable intervals and additional metrics

--- sample_feature_2.py
+++ sample_feature_2.py
@@ -1,16 +1,31 @@
 # Sample feature 2 - Agent monitoring tool
 import time
 
-def monitor_agent_performance(agent_id, duration=60):
-    """Monitor agent performance metrics over time."""
+def monitor_agent_performance(agent_id, duration=60, interval=1):
+    """Monitor agent performance metrics over time.
+    
+    Args:
+        agent_id: Unique identifier for the agent
+        duration: Monitoring duration in seconds
+        interval: Sampling interval in seconds
+    
+    Returns:
+        List of metric dictionaries
+    """
     metrics = []
-    for i in range(duration):
+    sample_count = duration // interval
+    
+    for i in range(sample_count):
         # Simulate metrics collection
         cpu_usage = 45 + (i % 20)
         memory_usage = 70 + (i % 15)
+        response_time = 100 + (i % 50)
+        
         metrics.append({
-            'timestamp': time.time() + i,
+            'timestamp': time.time() + i * interval,
+            'agent_id': agent_id,
             'cpu': cpu_usage,
-            'memory': memory_usage
+            'memory': memory_usage,
+            'response_time': response_time
         })
     return metrics
