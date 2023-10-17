73a0d8370f7f61cdc9a457bcd880bb20c3f33363
wangjian <zgsddzwj@163.com>
2023-10-17 15:20:00 +0800

    Enhance data processing utility with multiple transformation options

--- sample_feature_1.py
+++ sample_feature_1.py
@@ -1,7 +1,22 @@
 # Sample feature 1 - Data processing utility
-def process_data(data):
-    """Process input data and return transformed results."""
-    return [item.upper() for item in data if item]
+def process_data(data, transformation="upper"):
+    """Process input data and return transformed results.
+    
+    Args:
+        data: List of strings to process
+        transformation: Type of transformation ('upper', 'lower', 'title')
+    
+    Returns:
+        List of transformed strings
+    """
+    if transformation == "upper":
+        return [item.upper() for item in data if item]
+    elif transformation == "lower":
+        return [item.lower() for item in data if item]
+    elif transformation == "title":
+        return [item.title() for item in data if item]
+    else:
+        raise ValueError(f"Unknown transformation: {transformation}")
 
 if __name__ == "__main__":
     test_data = ["hello", "world", "agent", "ops"]
